97de3a64a220551dfe6c01b9a69d94ace92e9a4a
Jason Joshi <joshij@usc.edu>
2023-03-03 16:49:35 -0800

    Finished Part 1!

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <iostream>
 #endif
 
 #include "equal-paths.h"
@@ -8,11 +8,32 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+int processLeaves(Node* root, bool& l);
 
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+    bool lengthsEqual = true;
+    processLeaves(root, lengthsEqual);
+    return lengthsEqual;
 }
 
+int processLeaves(Node* root, bool& l){
+    if (root == nullptr){
+        return 0;
+    }
+    int rootL = processLeaves(root->left, l);
+    int rootR = processLeaves(root->right, l);
+    if (root->left != nullptr && root->right != nullptr){
+        if (rootL != rootR){
+            l = false;
+            return -1;
+        }
+        return ++rootR;
+    }
+    else if (root->left != nullptr && root->right == nullptr){
+        return rootL + 1;
+    } else if (root->left == nullptr && root->right != nullptr){
+        return rootR + 1;
+    }
+}
